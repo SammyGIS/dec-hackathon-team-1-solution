4fa4a3a8421fba6c74d5ef4a38c191a4885c8a32
SammyGIS <adedoyinsamuel25@gmail.com>
2024-07-20 18:07:22 +0100

    function fecthing data

--- main.py
+++ main.py
@@ -3,9 +3,22 @@
 # Date:
 #************************************************************************
 import requests
-import pandas as pd
-import json
-import psycopg2
 
+restcountries_url = "https://restcountries.com/v3.1/all"
+
+
+def get_data(url: str) -> dict:
+    # Send a GET request to the specified URL
+    request = requests.get(url)
+    
+    # Check if the request was successful (status code 200)
+    if request.status_code == 200:
+        # Parse the response content as JSON
+        response = request.json()
+        # Return the parsed JSON data
+        return response
+    else:
+        # If the request was not successful, return an error message
+        return 'Error fetching data'
 
 
